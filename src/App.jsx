2dbe5d72cd65efc32fb618ea2219b469aa5dc0e3
rafacosta87 <costarafa87@outlook.com>
2025-08-22 17:49:31 -0300

    implementado scroll em tarefas, responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 //não conseguimos deletar com filter
+//da onde esta vindo tamanho da fonte
+//se tem uma maneira melhor de fazer a responsividade
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './index.css'
 import Tarefa from "./components/Tarefa"
 import { X } from "lucide-react"
 
 function App() {
-  const [tarefas, setTarefas] = useState([])
+  const [tarefas, setTarefas] = useState(JSON.parse(localStorage.getItem("tarefas")))
   const [tarefa, setTarefa] = useState("")
   const [erro, setErro] = useState(null)
 
+useEffect(()=>{
+if(tarefas){
+  localStorage.setItem("tarefas", JSON.stringify(tarefas))
+}
+},[tarefas])
+
   const adicionarTarefa = () => {
-    if(tarefa.trim() == '')  { 
+    if (tarefa.trim() == '') {                                                                //se tarefa for string vazia, ou apemas espaços em branco , entrara no erro abaixo. lembrando que a função do trim e tirar o espaços em brancos
       setErro("Campo Obrigatório")
-      return 
+      return
     }
-    if(tarefas.filter(t => t.texto == tarefa ).length > 0 ) {
+    if (tarefas.filter(t => t.texto == tarefa).length > 0) {                                 //verificando se ja tem a tarefa no array, caso exista dara erro   
       setErro("Tarefa ja existe")
       return
     }
 
-      setTarefas([...tarefas, { texto: tarefa, createdAt: new Date(), done: false }])
-      setTarefa("")                                                                             
-     
+    setTarefas([...tarefas, { texto: tarefa, createdAt: new Date(), done: false }])
+    setTarefa("")                                                                           //limpando o input, para receber a próxima tarefa                           
+
   }
 
   const deletarTarefa = (id) => {
@@ -45,59 +53,66 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col gap-4 justify-center items-center h-screen '>
-        {tarefas.length > 0 ? (<h1 className="text-3xl">Lista De Tarefas</h1>) : null}
+      <div className='flex flex-col gap-4 justify-between items-center h-screen overflow-hidden m-4'>
+        <div className="flex flex-col gap-4 justify-center flex-1 items-center overflow-auto w-full">
 
-        <ul className="flex flex-col gap-2">
-          {tarefas.map((tarefa, id) =>
-            <Tarefa
-              key={id}
-              text={tarefa.texto}
-              createdAt={tarefa.createdAt}
-              done={tarefa.done}
-              onDelete={() => deletarTarefa(id)}
-              onToggleState={() => alternarEstado(id)} />
-          )}
-        </ul>
+          {tarefas.length > 0 ? (<h1 className="text-3xl">Lista De Tarefas</h1>) : null}
 
-        <div className="flex flex-row gap-2 ">
-          <input
-            type="text"
-            className="border border-gray-400 rounded-xl  px-[20px] py-[5px]  placeholder hover:shadow-md"
-            value={tarefa}
-            onChange={e => setTarefa(e.target.value)}
-            placeholder="Digite uma tarefa"
-            onKeyDown={(e) => {                                                         /* para ao digitarmos o valor no input , apertar a tecla enter , e salvar a tarefa*/
-              if (e.code == "Enter") {
-                adicionarTarefa()
-              }
-            }
-            }
-          />
-
-          <button
-            className=
-            "bg-green-500 p-2.5 rounded-xl hover:bg-green-600 active:bg-green-700 text-white px-[20px] py-[5px] font-bold cursor-pointer hover:shadow-md"
-            onClick={adicionarTarefa}
-          >
-            Adicionar
-          </button>
-          <button
-            className=
-            "bg-red-600 p-2.5 rounded-xl hover:bg-red-700 active:bg-red-800 text-white px-[30px] py-[5px] font-bold cursor-pointer hover:shadow-md"
-            onClick={limparTarefas}>
-            Limpar
-          </button>
+          <ul className="flex flex-col gap-2  overflow-auto w-full max-w-[500px]">
+            {tarefas.map((tarefa, id) =>
+              <Tarefa
+                key={id}
+                text={tarefa.texto}
+                createdAt={tarefa.createdAt}
+                done={tarefa.done}
+                onDelete={() => deletarTarefa(id)}
+                onToggleState={() => alternarEstado(id)} />
+            )}
+          </ul>
         </div>
+        <div className="flex flex-col gap-4 justify-center items-center w-full h-[30vh]">
 
-        {erro &&
-          <div className="flex flex-row gap-2">
-            <span className="text-red-500 ">{erro}</span>
-            <X
-            onClick={() => {setErro(null)}}
-            className="cursor-pointer p-1 rounded-full hover:bg-gray-100"/>
+          <div className="flex flex-col gap-2 sm:flex-row">
+            <input
+              type="text"
+              className="border border-gray-400 rounded-xl  px-[20px] py-[5px]  placeholder hover:shadow-md"
+              value={tarefa}
+              onChange={e => setTarefa(e.target.value)}
+              placeholder="Digite uma tarefa"
+              onKeyDown={(e) => {                                                         /* para ao digitarmos o valor no input , apertar a tecla enter , e salvar a tarefa*/
+                if (e.code == "Enter") {
+                  adicionarTarefa()
+                }
+              }
+              }
+            />
+            <div className="flex flex-row gap-2 justify-center items-center">
+
+              <button
+                className=
+                "bg-green-500 rounded-xl hover:bg-green-600 active:bg-green-700 text-white px-[20px] py-[5px] font-bold cursor-pointer hover:shadow-md"
+                onClick={adicionarTarefa}
+              >
+                Adicionar
+              </button>
+              <button
+                className=
+                "bg-red-600 rounded-xl hover:bg-red-700 active:bg-red-800 text-white px-[30px] py-[5px] font-bold cursor-pointer hover:shadow-md"
+                onClick={limparTarefas}>
+                Limpar
+              </button>
+            </div>
           </div>
-        }
+
+          {erro &&
+            <div className="flex flex-row gap-2">
+              <span className="text-red-500 text-[12px]">{erro}</span>
+              <X
+                onClick={() => { setErro(null) }}
+                className="cursor-pointer p-1.5 rounded-full hover:bg-gray-100" />
+            </div>
+          }
+        </div>
       </div>
     </>
   )
--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -1,7 +1,31 @@
-//tivemos dificuldade em deixar um elemento na ponta esquerda e outro na ponta direita
-import {Trash2, Check, X} from 'lucide-react'
- 
+
+//pergunta o papel flex-wrao breack normal no código , pq funciona da mesma maneira sem eles. E se eles precisam atuar em conjunto
+import { Trash2, Check, X } from 'lucide-react'
+import { useEffect, useMemo, useState } from 'react'
+
 function Tarefa({ done, text, createdAt, onDelete, onToggleState }) {
+  const [timeAgo, setTimeAgo] = useState(0)
+  const displayTime = useMemo(() => {
+    const hours = parseInt(timeAgo / 3600)
+
+    const minutes = parseInt((timeAgo % 3600) / 60)
+    if (hours > 0) {
+      return `${hours} hora(s)`
+    } else if (minutes > 0) {
+      return `${minutes} minuto(s)`
+    } else {
+      return "poucos segundos"
+    }
+  }, [timeAgo])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeAgo((new Date() - createdAt) / 1000)
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <>
@@ -9,47 +33,47 @@ function Tarefa({ done, text, createdAt, onDelete, onToggleState }) {
 
       <li
         className=
-        "flex justify-between items-center border border-gray-500 p-4 rounded-xl w-[500px] hover:shadow-md overflow-hidden"
+        "flex justify-center items-center border border-gray-500 p-4 rounded-xl w-full hover:shadow-md "
       >
         <div className="flex flex-col gap-2 text-left w-full flex-wrap">
 
           <p className="font-bold text-xl break-normal"
-            style={{textDecoration: done ? "line-through" : "unset"  , color: done ?  "gray" : "black"  }}>
+            style={{ textDecoration: done ? "line-through" : "unset", color: done ? "gray" : "black" }}>
             {text}
           </p>
-          <span className="text-sm text-gray-600 ">
-            {createdAt.toLocaleString()}                                                  {/*convertendo o createdAt para string*/}
+          <span className="text-sm text-gray-600 text-[12px]">
+            Criado há {displayTime}                                                  
 
           </span>
         </div>
-        
-<div className='flex flex-row items-center' >
-        <Trash2
-        height={35}
-        width={35}
-        className=" cursor-pointer rounded-full hover:bg-gray-200 p-[10px] text-gray-500 active:text-gray-700"
-        onClick={onDelete}
-        />
-        {
-          done ?
-          <Check
-          height={30}
-          width={30}
-          className='cursor-pointer rounded-full hover:bg-gray-200 p-[10px] text-green-500 active:text-green-700'
-          style={{ padding: '5px' }}
-          onClick={onToggleState}
-          
+
+        <div className='flex flex-row items-center w-full justify-end' >
+          <Trash2
+            height={35}
+            width={35}
+            className=" cursor-pointer rounded-full hover:bg-gray-100 p-[10px] text-gray-500 active:text-gray-700"
+            onClick={onDelete}
           />
-          :
-          <X
-          height={30}
-          width={30}
-          className='cursor-pointer rounded-full hover:bg-gray-200 p-[10px] text-red-500 active:text-red-700'
-          style={{ padding: '5px' }}
-          onClick={onToggleState}
-          />
-        }
-</div>     
+          {
+            done ?
+              <Check
+                height={30}
+                width={30}
+                className='cursor-pointer rounded-full hover:bg-gray-100 p-[10px] text-green-500 active:text-green-700'
+                style={{ padding: '5px' }}
+                onClick={onToggleState}
+
+              />
+              :
+              <X
+                height={30}
+                width={30}
+                className='cursor-pointer rounded-full hover:bg-gray-100 p-[10px] text-red-500 active:text-red-700'
+                style={{ padding: '5px' }}
+                onClick={onToggleState}
+              />
+          }
+        </div>
       </li>
 
     </>
